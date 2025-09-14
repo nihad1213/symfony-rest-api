25f85a8171bf24212d5e0abd8030bcb8b8708460
Nihad <nihad.nemetli@gmail.com>
2025-09-14 20:38:22 +0400

    index method fix

--- src/Controller/Api/ProductController.php
+++ src/Controller/Api/ProductController.php
@@ -7,13 +7,18 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 final class ProductController extends AbstractController
 {
     #[Route('/api/products', methods: ['GET'])]
-    public function index(EntityManagerInterface $em): JsonResponse
+    public function index(EntityManagerInterface $em, SerializerInterface $serializer): JsonResponse
     {
         $products = $em->getRepository(Product::class)->findAll();
-        return $this->json($products);
+        $json_content = $serializer->serialize($products, 'json');
+
+        // return $this->json($products);
+        return JsonResponse::fromJsonString($json_content);
     }
 }
